d1587e2bcd4eb74b57b8aecd03f154fa1941fce1
Roop Kumar Lala <ui20cs60@iiitsurat.ac.in>
2023-12-22 23:32:52 +0530

    Revert "Add Via Console"
    This reverts commit 7369beb6c4b4fcac605fd81316e0eccec6703400.

--- address/src/main/java/com/addressbook/Main.java
+++ address/src/main/java/com/addressbook/Main.java
@@ -1,37 +1,10 @@
 package com.addressbook;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome To Address Book");
-        
-        // Console Input:-
-        String firstName, lastName, address, city, state, email;
-        int zip, phoneNo;
 
-        System.out.print("First Name: ");
-        firstName = scanner.nextLine();
-        System.out.print("Last Name: ");
-        lastName = scanner.nextLine();
-        System.out.print("Address: ");
-        address = scanner.nextLine();
-        System.out.print("City: ");
-        city = scanner.nextLine();
-        System.out.print("State: ");
-        state = scanner.nextLine();
-        System.out.print("Zip: ");
-        zip = scanner.nextInt();
-        scanner.nextLine(); // Consume New Line
-        System.out.print("Email: ");
-        email = scanner.nextLine();
-        System.out.print("Contact: ");
-        phoneNo = scanner.nextInt();
-        scanner.nextLine(); // Consume New Line
-
-        Contact person = new Contact(firstName, lastName, address, city, state, email, zip, phoneNo);
-        scanner.close();
+        Contact c1 = new Contact("Roop", "Lala", "Adajan", "Surat", "Gujarat", "abc@gmail.com", 112233, 111222333);
     }
 }
 
@@ -39,7 +12,6 @@ class Contact{
     public String firstName, lastName, address, city, state, email;
     public int zip, phoneNo;
 
-    Contact(){}
     Contact(String firstName, String lastName, String address, String city, String state, String email, int zip, int phoneNo){
         this.firstName = firstName;
         this.lastName = lastName;
