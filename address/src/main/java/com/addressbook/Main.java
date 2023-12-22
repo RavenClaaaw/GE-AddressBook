6605049d1f130ce7047b99383c19ce85b89cffdb
Roop Kumar Lala <ui20cs60@iiitsurat.ac.in>
2023-12-22 23:53:31 +0530

    Edit

--- address/src/main/java/com/addressbook/Main.java
+++ address/src/main/java/com/addressbook/Main.java
@@ -1,37 +1,50 @@
 package com.addressbook;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome To Address Book");
-        
-        // Console Input:-
-        String firstName, lastName, address, city, state, email;
-        int zip, phoneNo;
+        ArrayList<Contact> addressBook = new ArrayList<>();
 
-        System.out.print("First Name: ");
-        firstName = scanner.nextLine();
-        System.out.print("Last Name: ");
-        lastName = scanner.nextLine();
-        System.out.print("Address: ");
-        address = scanner.nextLine();
-        System.out.print("City: ");
-        city = scanner.nextLine();
-        System.out.print("State: ");
-        state = scanner.nextLine();
-        System.out.print("Zip: ");
-        zip = scanner.nextInt();
-        scanner.nextLine(); // Consume New Line
-        System.out.print("Email: ");
-        email = scanner.nextLine();
-        System.out.print("Contact: ");
-        phoneNo = scanner.nextInt();
-        scanner.nextLine(); // Consume New Line
+        while(true){
+            System.out.println("1. ADD \n2. EDIT \n3. DELETE \n4. Display");
+            System.out.print("ENTER OPTION:- ");
+            int option = scanner.nextInt();
+            scanner.nextLine();
 
-        Contact person = new Contact(firstName, lastName, address, city, state, email, zip, phoneNo);
-        scanner.close();
+            switch (option) {
+                case 1:
+                    Contact person = new Contact();
+                    addressBook.add(person);
+                    break;
+
+                case 2:
+                    String firstNameE = scanner.nextLine();
+                    String lastNameE = scanner.nextLine();
+                    for(int i=0; i<addressBook.size(); i++){
+                        if(addressBook.get(i).firstName == firstNameE && addressBook.get(i).lastName == lastNameE){
+                            Contact personEdit = new Contact();
+                            addressBook.set(i, personEdit);
+                        }
+                    }
+                    break;
+                
+                case 3:
+                    
+
+                default:
+                    for(int i=0; i<addressBook.size(); i++){
+                        System.out.println(addressBook.get(i).firstName + " " + addressBook.get(i).lastName);
+                    }
+                    break;
+            }
+
+        }
+
+        // scanner.close();
     }
 }
 
@@ -39,7 +52,31 @@ class Contact{
     public String firstName, lastName, address, city, state, email;
     public int zip, phoneNo;
 
-    Contact(){}
+    Contact(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("First Name: ");
+        this.firstName = scanner.nextLine();
+        System.out.print("Last Name: ");
+        this.lastName = scanner.nextLine();
+        System.out.print("Address: ");
+        this.address = scanner.nextLine();
+        System.out.print("City: ");
+        this.city = scanner.nextLine();
+        System.out.print("State: ");
+        this.state = scanner.nextLine();
+        System.out.print("Zip: ");
+        this.zip = scanner.nextInt();
+        scanner.nextLine(); // Consume New Line
+        System.out.print("Email: ");
+        this.email = scanner.nextLine();
+        System.out.print("Contact: ");
+        this.phoneNo = scanner.nextInt();
+        scanner.nextLine(); // Consume New Line
+
+        // scanner.close();
+    }
+
     Contact(String firstName, String lastName, String address, String city, String state, String email, int zip, int phoneNo){
         this.firstName = firstName;
         this.lastName = lastName;
