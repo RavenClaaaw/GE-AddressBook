c1d43a35a7fabbfb2c91442aee20fe439a0c0820
Roop Kumar Lala <ui20cs60@iiitsurat.ac.in>
2023-12-25 13:21:50 +0530

    Add a person

---
+++ address/src/main/java/com/addressbook/Contact.java
@@ -0,0 +1,43 @@
+package com.addressbook;
+import java.util.Scanner;
+
+public class Contact {
+    public String firstName, lastName, address, city, state, email;
+    public int zip, phoneNo;
+
+    Contact(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("First Name: ");
+        this.firstName = scanner.nextLine();
+        System.out.print("Last Name: ");
+        this.lastName = scanner.nextLine();
+        System.out.print("Address: ");
+        this.address = scanner.nextLine();
+        System.out.print("City: ");
+        this.city = scanner.nextLine();
+        System.out.print("State: ");
+        this.state = scanner.nextLine();
+        System.out.print("Zip: ");
+        this.zip = scanner.nextInt();
+        scanner.nextLine(); // Consume New Line
+        System.out.print("Email: ");
+        this.email = scanner.nextLine();
+        System.out.print("Contact: ");
+        this.phoneNo = scanner.nextInt();
+        scanner.nextLine(); // Consume New Line
+
+        // scanner.close();
+    }
+
+    Contact(String firstName, String lastName, String address, String city, String state, String email, int zip, int phoneNo){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.email = email;
+        this.zip = zip;
+        this.phoneNo = phoneNo;
+    } 
+}
